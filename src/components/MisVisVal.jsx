e3f872b79f75ddf8ca6d2de28a861918b738c5b6
AlbertoDPM30 <albertodpm2000@gmail.com>
2024-03-04 21:52:42 -0400

    M=MisVisVal.jsx margin fixed

--- src/components/MisVisVal.jsx
+++ src/components/MisVisVal.jsx
@@ -1,14 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Grid, CardContent, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
 
 export const MisVisVal = () => {
   return (
     <>
-      <Grid 
-      paddingY={5}
-      paddingX={2}
-      container
-      marginY={10}>
+      <Grid paddingY={5} paddingX={2} container marginY={10}>
         <Grid paddingBottom={2} marginBottom={3}>
           <Typography variant="h2" color="initial">
             Misión, Visión y Valores
@@ -16,12 +12,15 @@ export const MisVisVal = () => {
         </Grid>
         <Grid
           container
-          spacing={2}
+          width={"100%"}
           direction="row"
           justifyContent="space-evenly"
           wrap="wrap"
+          maX={5}
+        >
+          <CardContent
+            sx={"background:#2d2d2d; border-radius:15px;margin:25px;"}
           >
-          <CardContent sx={'background:#2d2d2d; border-radius:15px;margin:25px;'}>
             <Typography
               paddingY={2}
               align="center"
@@ -30,11 +29,29 @@ export const MisVisVal = () => {
             >
               Misión
             </Typography>
-            <Typography width={"350px"} variant="body1" color="#c3c3c3" align="center">
-            La Unidad Educativa "RAFAEL URDANETA" se compromete a crear un nuevo ciudadano con valores de paz, solidaridad, integridad e identidad nacional. Este ciudadano valorará sus deberes, derechos y el lenguaje como herramienta de comunicación y convivencia. Participará en espacios que potencien su desarrollo y transformación, centrada en el ser social y la participación protagónica. La nueva escuela, como centro comunitario, optimizará espacios de acción y brindará integración escuela-familia-comunidad. Se busca mejorar la capacidad para la transformación de hábitos de salud, prevención de enfermedades y normas de convivencia que fortalezcan la formación integral del personal escolar.
+            <Typography
+              width={"350px"}
+              variant="body1"
+              color="#c3c3c3"
+              align="center"
+            >
+              La Unidad Educativa "RAFAEL URDANETA" se compromete a crear un
+              nuevo ciudadano con valores de paz, solidaridad, integridad e
+              identidad nacional. Este ciudadano valorará sus deberes, derechos
+              y el lenguaje como herramienta de comunicación y convivencia.
+              Participará en espacios que potencien su desarrollo y
+              transformación, centrada en el ser social y la participación
+              protagónica. La nueva escuela, como centro comunitario, optimizará
+              espacios de acción y brindará integración
+              escuela-familia-comunidad. Se busca mejorar la capacidad para la
+              transformación de hábitos de salud, prevención de enfermedades y
+              normas de convivencia que fortalezcan la formación integral del
+              personal escolar.
             </Typography>
           </CardContent>
-          <CardContent sx={'background:#2d2d2d; border-radius:5px;margin:25px;'}>
+          <CardContent
+            sx={"background:#2d2d2d; border-radius:5px;margin:25px;"}
+          >
             <Typography
               paddingY={2}
               align="center"
@@ -43,11 +60,26 @@ export const MisVisVal = () => {
             >
               Visión
             </Typography>
-            <Typography width={"350px"} variant="body1" color="#c3c3c3" align="center">
-            La Unidad Educativa "RAFAEL URDANETA" busca una formación integral y continua del ciudadano. Se enfoca en la triada social: individuo, familia y sociedad. Su objetivo es la transformación social, fomentando valores como la igualdad, solidaridad, libertad, participación y democracia. La escuela busca insertarse en el contexto socio-económico y cultural, modelando un ciudadano capaz de transformar su entorno social a través de la participación, creación e innovación, incluyendo a todos los niños y niñas sin escolaridad.
+            <Typography
+              width={"350px"}
+              variant="body1"
+              color="#c3c3c3"
+              align="center"
+            >
+              La Unidad Educativa "RAFAEL URDANETA" busca una formación integral
+              y continua del ciudadano. Se enfoca en la triada social:
+              individuo, familia y sociedad. Su objetivo es la transformación
+              social, fomentando valores como la igualdad, solidaridad,
+              libertad, participación y democracia. La escuela busca insertarse
+              en el contexto socio-económico y cultural, modelando un ciudadano
+              capaz de transformar su entorno social a través de la
+              participación, creación e innovación, incluyendo a todos los niños
+              y niñas sin escolaridad.
             </Typography>
           </CardContent>
-          <CardContent sx={'background:#2d2d2d; border-radius:5px;margin:25px;'}>
+          <CardContent
+            sx={"background:#2d2d2d; border-radius:5px;margin:25px;"}
+          >
             <Typography
               paddingY={2}
               align="center"
@@ -56,8 +88,19 @@ export const MisVisVal = () => {
             >
               Valores
             </Typography>
-            <Typography width={"350px"} variant="body1" color="#c3c3c3" align="center">
-            La Unidad Educativa "RAFAEL URDANETA" busca una educación integral de calidad que forme a ciudadanos responsables, cumplidos con sus deberes y derechos, y conscientes de su salud integral. La educación se basa en la ética, la moral y valores sólidos que modelen a los estudiantes como futuros líderes de una patria soberana y socialista, con equidad e igualdad para todos, sin discriminación.
+            <Typography
+              width={"350px"}
+              variant="body1"
+              color="#c3c3c3"
+              align="center"
+            >
+              La Unidad Educativa "RAFAEL URDANETA" busca una educación integral
+              de calidad que forme a ciudadanos responsables, cumplidos con sus
+              deberes y derechos, y conscientes de su salud integral. La
+              educación se basa en la ética, la moral y valores sólidos que
+              modelen a los estudiantes como futuros líderes de una patria
+              soberana y socialista, con equidad e igualdad para todos, sin
+              discriminación.
             </Typography>
           </CardContent>
         </Grid>
