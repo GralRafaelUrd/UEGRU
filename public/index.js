975d0161ba788fa72749e102259ec614727a3721
AlbertoDPM30 <albertodpm2000@gmail.com>
2024-03-04 21:36:50 -0400

    new images routes old method

--- public/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./GRU.jpg";
-export * from "./logo.jpg";
-export * from "./logo.escuela.jpg";
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,9 @@
 import Carousel from "react-material-ui-carousel";
 import { Item } from "./Item";
 import { Grid } from "@mui/material";
-import img from "../imgRoutes.js";
+import about from "../../public/Ue/about.jpeg";
+import gru from "../../public/GRU.jpg";
+import lb from "../../public/Ue/LB.jpg";
 
 export const Slider = () => {
   const items = [
@@ -9,19 +11,19 @@ export const Slider = () => {
       id: 1,
       name: "Acerca de U.E. General Rafael Urdaneta",
       description: `Se Identifica como una institución pública, dependiente del órgano rector Ministerio del Poder Popular para la Educacion Venezolana, que integra a todos los niños, niñas en edad preescolar y escolar, al servicio de las comunidades que giran en su entorno, de dependencia Nacional integrada en el territorio escolar N° 81405005 del Centro de desarrollo Escolar Rafael Urdaneta.`,
-      img: img.about,
+      img: about,
     },
     {
       id: 2,
       name: "General Rafael Urdaneta",
       description: `Rafael José Urdaneta Farías (La Cañada de Urdaneta, 24 de octubre de 1788-París, 23 de agosto de 1845) fue un militar, prócer, estratega y político venezolano. Ejerció de facto la presidencia de la Gran Colombia entre el 4 de septiembre de 1830 al 2 de mayo de 1831, asumiendo el poder en representación de Simón Bolívar.`,
-      img: img.gru,
+      img: gru,
     },
     {
       id: 3,
       name: "Luis Beltran Prieto Figueroa",
       description: `La escuela no puede ser reflejo de la sociedad que tenemos; la escuela debe ser reflejo de la sociedad que queremos`,
-      img: img.lb,
+      img: lb,
     },
   ];
 
